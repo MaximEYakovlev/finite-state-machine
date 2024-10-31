1fb77015adb30715482ca58fa55745070136345a
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2024-10-31 09:25:02 +0300

    state and transition logic implementation (class OnState)

---
+++ TS/OnState.ts
@@ -0,0 +1,20 @@
+import { IState } from './IState';
+import { IStateMachine } from './IStateMachine';
+
+export class OnState implements IState {
+    constructor(private machine: IStateMachine) { }
+
+    onEnter() {
+        console.log('Entering On State');
+    }
+
+    onExit() {
+        console.log('Exiting On State');
+    }
+
+    getTransitions(): Map<string, () => void> {
+        return new Map([
+            ['press', () => this.machine.setState('Blink')]
+        ]);
+    }
+}
