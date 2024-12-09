925a975116f534fa603a195062cee5f0414d465d
Maxim Yakovlev <it.trainee@yandex.ru>
2024-12-09 22:08:51 +0300

    chain of actions / state changing / transitions

--- index.ts
+++ index.ts
@@ -3,4 +3,15 @@ import { StateMachine } from "./TS/StateMachine";
 
 const ledController = new StateMachine('Off');
 
+// entering 'off' state
 console.log(`Initial state: ${ledController.getState()}`);
+// exiting 'off' state
+
+// entering 'on' state
+// exiting 'on' state
+
+// entering 'blink' state
+// exiting 'blink' state
+
+// entering 'off' state
+// exiting 'off' state
