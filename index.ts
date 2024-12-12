ee73c78419494c645dc94a57d1b79a620b5b2576
Maxim Yakovlev <it.trainee@yandex.ru>
2024-12-12 22:14:56 +0300

    usage update: getting state

--- index.ts
+++ index.ts
@@ -4,16 +4,22 @@ import { StateMachine } from "./TS/StateMachine";
 const ledController = new StateMachine('Off');
 
 // entering 'off' state
-console.log(`Initial state: ${ledController.getState()}`);
+console.log(`initial state: ${ledController.getState()}`);
 // exiting 'off' state
 
 // entering 'on' state
 ledController.setState('On');
 // exiting 'on' state
 
+console.log(`state: ${ledController.getState()}`);
+
 // entering 'blink' state
 ledController.setState('Blink');
 // exiting 'blink' state
 
+console.log(`state: ${ledController.getState()}`);
+
 // entering 'off' state
 ledController.setState('Off');
+
+console.log(`state: ${ledController.getState()}`);
